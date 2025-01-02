77fe2a1d8b613cf468fbe7567c798fbe87e83a76
varunkumar <varunkumar02021@gmail.com>
2025-01-02 11:27:08 +0530

    commit 2-1-25 11-26

--- app.js
+++ app.js
@@ -1,52 +1,42 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
-const app = express();
 const path = require("path");
-const port = 5000;
+const app = express();
+const port = 8080;
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
-app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async (req, res) => {
   const amazonURL =
     "https://www.amazon.in/gp/browse.html?node=4092115031&ref_=nav_em_sbc_tvelec_gaming_consoles_0_2_9_12";
 
-  let data = [];
-  try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      executablePath: "/usr/bin/google-chrome-stable",
-    });
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    await page.goto(amazonURL, { waitUntil: "domcontentloaded" });
+  await page.goto(amazonURL);
 
-    data = await page.$$eval(".a-list-item", (elements) => {
-      console.log(elements);
+  const data = await page.$$eval(
+    ".a-section.octopus-pc-card-content .a-list-item",
+    (elements) => {
       return elements.map((el) => ({
         title:
-          el.querySelector(".octopus-pc-asin-title")?.innerText.trim() ||
-          "No Title Available",
+          el.querySelector(".octopus-pc-asin-title")?.innerText ||
+          "No title available",
         price:
-          el.querySelector(".a-price .a-offscreen")?.innerText.trim() ||
-          "No Price Available",
-        imageURL:
-          el.querySelector("img")?.src || "https://via.placeholder.com/150",
+          el.querySelector(".a-price .a-offscreen")?.innerText ||
+          "No price available",
+        imageURL: el.querySelector("img")?.src || "No image available",
       }));
-    });
+    }
+  );
 
-    await browser.close();
-  } catch (error) {
-    console.error("Error during scraping:", error.message);
-  }
+  await browser.close();
 
-  // console.log("Scraped Data:", data);
-
-  // Render the EJS template
   res.render("index", { data });
 });
 
 app.listen(port, () => {
-  console.log(`App listening at port ${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
